c6ce6093fbfeaba0d3959c780e86686ccdeec4ae
Arkadiusz Juszczyk <arek@lifeinmobile.com>
2018-02-01 21:23:18 +0100

    Added swagger docs.

--- app/Gnome.php
+++ app/Gnome.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(definition="Gnome", type="object", required={"name", "age", "strength"})
+ */
 class Gnome extends Model
 {
     protected $fillable = [
@@ -13,4 +16,28 @@ class Gnome extends Model
     protected $hidden = [
         'created_at', 'updated_at'
     ];
+
+    /**
+     * @var number
+     * @SWG\Property()
+     */
+    public $id;
+
+    /**
+     * @var string
+     * @SWG\Property()
+     */
+    public $name;
+
+    /**
+     * @var number
+     * @SWG\Property()
+     */
+    public $age;
+
+    /**
+     * @var number
+     * @SWG\Property()
+     */
+    public $strength;
 }
--- app/Http/Controllers/GnomesController.php
+++ app/Http/Controllers/GnomesController.php
@@ -8,18 +8,95 @@ use Illuminate\Support\Facades\Validator;
 
 class GnomesController extends Controller
 {
+    /**
+     * @SWG\Get(
+     *     path="/gnomes",
+     *     description="Returns all gnomes.",
+     *     produces={"application/json"},
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Gnomes",
+     *         @SWG\Schema(
+     *             type="json",
+     *             @SWG\Items(ref="#/definitions/Gnome")
+     *         ),
+     *     )
+     * )
+     */
     public function getGnomes()
     {
         $gnomes = Gnome::all();
         return response()->json($gnomes);
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/gnome/{id}",
+     *     description="Returns one specified gnome.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         description="ID of gnome to retrieve.",
+     *         format="int64",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Gnome",
+     *         @SWG\Schema(
+     *             type="json",
+     *             @SWG\Items(ref="#/definitions/Gnome")
+     *         ),
+     *     )
+     * )
+     */
     public function getGnome(int $id)
     {
         $gnome = Gnome::find($id);
         return response()->json($gnome, $gnome ? 200 : 404);
     }
 
+    /**
+     * @SWG\Post(
+     *     path="/gnome",
+     *     description="Creates a new gnome.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="body",
+     *         description="Name of the gnome.",
+     *         required=true,
+     *         format="string",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Parameter(
+     *         name="age",
+     *         in="body",
+     *         description="Age of the gnome.",
+     *         required=true,
+     *         format="int64",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Parameter(
+     *         name="strength",
+     *         in="body",
+     *         description="Strength of the gnome.",
+     *         required=true,
+     *         format="int64",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Gnomes",
+     *         @SWG\Schema(
+     *             type="json",
+     *             @SWG\Items(ref="#/definitions/Gnome")
+     *         ),
+     *     )
+     * )
+     */
     public function createGnome(Request $request)
     {
         $data = $request->only(['name', 'age', 'strength']);
@@ -44,6 +121,53 @@ class GnomesController extends Controller
         return response()->json(['id' => $gnomeId], 201);
     }
 
+    /**
+     * @SWG\Put(
+     *     path="/gnome/{id}",
+     *     description="Updates the specified gnome.",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         description="ID of gnome to update.",
+     *         format="int64",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="body",
+     *         description="Name of the gnome.",
+     *         required=false,
+     *         format="string",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Parameter(
+     *         name="age",
+     *         in="body",
+     *         description="Age of the gnome.",
+     *         required=false,
+     *         format="int64",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Parameter(
+     *         name="strength",
+     *         in="body",
+     *         description="Strength of the gnome.",
+     *         required=false,
+     *         format="int64",
+     *         @SWG\Schema(ref="#/definitions/Gnome")
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Gnomes",
+     *         @SWG\Schema(
+     *             type="json",
+     *             @SWG\Items(ref="#/definitions/Gnome")
+     *         ),
+     *     )
+     * )
+     */
     public function updateGnome(Request $request, int $id)
     {
         $data = $request->only(['name', 'age', 'strength']);
@@ -71,6 +195,24 @@ class GnomesController extends Controller
         return response()->json([], 404);
     }
 
+    /**
+     * @SWG\Delete(
+     *     path="/gnome/{id}",
+     *     description="Deletes the specified gnome.",
+     *     @SWG\Parameter(
+     *         description="ID of gnome to delete.",
+     *         format="int64",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Gnome deleted."
+     *     )
+     * )
+     */
     public function deleteGnome(int $id)
     {
         $gnome = Gnome::find($id);
